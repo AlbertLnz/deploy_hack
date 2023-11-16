ac6b84300ce4383b1e465e2e68b303438169c665
AlbertLnz <albert2000_lanza@hotmail.com>
2023-11-16 23:02:30 +0100

    7n deploy

--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,26 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
-use App\Http\Controllers\MultipolygonController;
 use App\Http\Controllers\Api\V1\InterventionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\MultipolygonsController;
 
 
 
-// Route::post('/register', [AuthController::class, 'register']);
-// Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/interventions', [InterventionController::class, 'index']);
-Route::get('/intervention/{id}', [InterventionController::class, 'show']);
+// Route::get('/intervention/{id}', [InterventionController::class, 'show']);
+Route::get('/intervention/{borough_id}', [InterventionController::class, 'showBorough']);
 
-// Route::middleware(['auth:api'])->group(function () {
+Route::middleware(['auth:api'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/intervention', [InterventionController::class, 'store']);
+    Route::put('/intervention/{id}', [InterventionController::class, 'update']);
+    Route::delete('/intervention/{id}', [InterventionController::class, 'destroy']);
+});
 
-// Route::post('/logout', [AuthController::class, 'logout']);
-Route::post('/intervention', [InterventionController::class, 'store']);
-Route::put('/intervention/{id}', [InterventionController::class, 'update']);
-Route::delete('/intervention/{id}', [InterventionController::class, 'destroy']);
-// });
-
-Route::get('/all', [MultipolygonController::class, 'getAllMultipolygons'])->name('api.getAllMultipolygons');
-Route::get('/boroughs', [MultipolygonController::class, 'getCoordsFromAllBorough'])->name('api.getBoroughsCoords');
+Route::get('/all', [MultipolygonsController::class, 'getAllMultipolygons'])->name('api.getAllMultipolygons');
+Route::get('/boroughs', [MultipolygonsController::class, 'getCoordsFromAllBorough'])->name('api.getBoroughsCoords');
